9f509199df76e14697f34b6babb57e61a093ac54
Harremking0 <chakibou1@hotmail.com>
2021-06-03 19:40:44 +0100

    fixed a typo

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -4,7 +4,7 @@ function Rules() {
     return (
         <div className="rules">
            <p style={{fontWeight:"bold"}}> Rules </p>
-            click on a charachter to begin, if you cick twice on the same charachter you'll loose the game . good luck!
+            click on a charachter to begin, if you click twice on the same charachter you'll loose the game . good luck!
         </div>
     )
 }
