a54fe564a1836657ed982fdcbf41001fcd397630
Harremking0 <chakibou1@hotmail.com>
2021-06-03 19:18:47 +0100

    aded Game container component and some mock  jsx to set the structure and sytling

---
+++ src/components/GameContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function GameContainer() {
+
+
+    return (
+        <div className="wrapper">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
+    )
+}
+
+export default GameContainer
