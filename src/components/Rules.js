0c7febb743135d884303bffd20ce0b3e5dda1c8f
Harremking0 <chakibou1@hotmail.com>
2021-06-03 18:34:03 +0100

    added rules component that displays the rules of the game

---
+++ src/components/Rules.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../style/Rules.css"
+function Rules() {
+    return (
+        <div className="rules">
+           <p style={{fontWeight:"bold"}}> Rules </p>
+            click on a charachter to begin, if you cick twice on the same charachter you'll loose the game . good luck!
+        </div>
+    )
+}
+
+export default Rules
