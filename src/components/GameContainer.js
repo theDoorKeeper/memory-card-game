28f09b5874eed7b6ea7c0c4473b84f6b7207279b
Harremking0 <chakibou1@hotmail.com>
2021-06-05 17:05:09 +0100

    rewrote the logic for Compare app , added a cache State and a Add to game cache to handle the scoring logic

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -3,38 +3,39 @@ import "../style/GameContainer.css"
 import {imageArray,startingArray }from './imageArray' 
 import randomNumber from './randomNumber';
 function GameContainer() {
-  
+    const [cacheArray,setCacheArray]=useState([])
     const [images,setimages] = useState([]);
     const [cards,setCards]=useState([])
     const [score,setScore]=useState(0)
 
 
-      const play = (cardID) => {
-       images.forEach(image=>{
-         if(image.id!==cardID){
-           setScore(prevScore=>prevScore+1)
-         }
-         else setScore(0)
-       })
+      const compareCard = (cardID) => {
+       if( cacheArray.includes(cardID) ){
+         setScore(0)
+       }
+       else {
+         setScore(prevScore=>prevScore+1)
+       }
       }
 
     const refreshId = (e) =>{  
       if (e.target!==e.currentTarget){
         let  array= [];
-        play(e.target.id)
         for (let i = 0; i < 6; i++) {   
-          console.log(array)
+          /* console.log(array) */
           let random = randomNumber(14,1);
                 array.push(...imageArray.filter( img => img.id === random) )
               
          }
         
          setimages(array);}
-         
-   
+    }
+
+    const addtoGameCache = (e)=>{
+      setCacheArray(previousArray => previousArray.concat(e.target.id))
     }
     useEffect(() => {  
-         console.log(images) 
+        /*  console.log(images)  */
          setCards( images.map(card=> <img key={card.index} id={card.id} src={card.src} className="card" alt="card" /> ) )
       }, [images])
 
@@ -45,7 +46,10 @@ function GameContainer() {
 
 
     return (
-        <div className="wrapper" onClick={(e)=>{refreshId(e)}}> {cards} score : {score}</div>
+        <div className="wrapper" onClick={(e)=>{
+          compareCard(e.target.id);
+          addtoGameCache(e);
+          refreshId(e)}}> {cards} score : {score}</div>
 
     )
 }
