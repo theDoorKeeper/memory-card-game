d0d4ffd3810860acb7147059d064e4526b62517a
Harremking0 <chakibou1@hotmail.com>
2021-06-05 01:19:59 +0100

    fixed the bug that made the array dispay more then 6 cards

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import "../style/GameContainer.css"
 import {imageArray,startingArray }from './imageArray' 
+import randomNumber from './randomNumber';
 function GameContainer() {
-
+  
     const [id,setId] = useState([]);
     const [cards,setCards]=useState([])
     
-    const refreshId = () =>{
-        let  array= [];
-       
-        for (let i = 0; i < 6; i++) {
-         if(id.length){
-            array.length=0;
-        }
-                array.push (...imageArray.filter( img =>img.id===(Math.floor(Math.random() * (14 - 1 + 1)) + 1) ) )
+    const refreshId = () =>{  
+      let  array= [];
+        for (let i = 0; i < 6; i++) {   
+          console.log(array)
+          let random = randomNumber(14,1);
+                array.push(...imageArray.filter( img => img.id === random) )
+              
          }
-         console.log('array',array)
         
-         setId(array)
+         setId(array);
+         
    
     }
     useEffect(() => {  
-        console.log(id)
-         setCards( id.map(card=> <img key={card.id} id={card.id} src={card.src} className="card" alt="card" /> ) )
+         console.log(id) 
+         setCards( id.map(card=> <img key={card.index} id={card.id} src={card.src} className="card" alt="card" /> ) )
       }, [id])
 
       useEffect(() => {
@@ -32,7 +32,7 @@ function GameContainer() {
 
 
     return (
-        <div className="wrapper" onClick={()=>{refreshId()}}> { cards ? cards : null} </div>
+        <div className="wrapper" onClick={()=>{refreshId()}}> {cards} </div>
 
     )
 }
