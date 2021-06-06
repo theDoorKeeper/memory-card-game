b12d037bb95cd16c971d5a3d4129dd0f2254da1c
Harremking0 <chakibou1@hotmail.com>
2021-06-06 02:02:59 +0100

    changed the JSX for esthetic purpouses

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import "../style/Score.css"
 function Score(props) {
     const {score,highScore} = props
     return (
-        <div className="scoreContainer">
-            Score : {score}
-            Higschore : {highScore}
+        <div className="scoreContainer" id="score">
+           <div className="score"> Score : {score} </div> 
+           <div className="highScore"> HighScore : {highScore}</div> 
         </div>
     )
 }
